b3a7347b27f78519278e2bc6189462cccbd554ab
matosjoaops <matosjoaops@protonmail.com>
2022-03-28 12:46:38 +0100

    Only one exercise left for lunch list

--- LunchList/app/src/main/java/com/example/lunchlist1/MainActivity.kt
+++ LunchList/app/src/main/java/com/example/lunchlist1/MainActivity.kt
@@ -1,21 +1,50 @@
 package com.example.lunchlist1
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.View
+import android.view.ViewGroup
 import android.widget.*
-import androidx.appcompat.view.menu.ListMenuItemView
+
+import android.widget.ArrayAdapter
+
 
 class MainActivity : AppCompatActivity() {
+    val rests = arrayListOf<Restaurant>()
+
+    inner class RestaurantAdapter: ArrayAdapter<Restaurant>(this@MainActivity, R.layout.row, rests) {
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val row = convertView ?: layoutInflater.inflate(R.layout.row, parent, false)
+            val r = rests[position]
+            row.findViewById<TextView>(R.id.rest_name).text = r.name
+            row.findViewById<TextView>(R.id.rest_address).text = r.address
+
+            val symbol = row.findViewById<ImageView>(R.id.type_image)
+            when (r.type) {
+                RestaurantType.SITDOWN -> symbol.setImageResource(R.drawable.ball_red)
+                RestaurantType.TAKEOUT -> symbol.setImageResource(R.drawable.ball_yellow)
+                RestaurantType.DELIVERY -> symbol.setImageResource(R.drawable.ball_green)
+                else -> symbol.setImageResource(R.drawable.ball_green)
+            }
+
+            return row
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.setIcon(R.drawable.rest_icon)
         supportActionBar?.setDisplayShowHomeEnabled(true)
         setContentView(R.layout.activity_main)
 
-        val rests = arrayListOf<Restaurant>();
-
-        val adapter by lazy { ArrayAdapter(this, android.R.layout.simple_list_item_1, rests) }
+        //val adapter by lazy { ArrayAdapter(this, android.R.layout.simple_list_item_1, rests) }
         val list = findViewById<ListView>(R.id.rests_list)
+        val adapter = RestaurantAdapter()
         list.adapter = adapter
 
         findViewById<Button>(R.id.save_btn).setOnClickListener{
